9488e7332b1c6a45e30b3d4fbbb53163f880d9a5
Viet Huynh <huynhviet98@gmail.com>
2024-06-06 01:22:09 +0700

    - adjust navbar in each page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,18 @@
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import PostDetails from "./pages/PostDetails";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
+        <Route path="/posts/post/:id" element={<PostDetails/>}/>
       </Routes>
-      <Footer />
     </div>
   );
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,21 @@
+import Footer from "../components/Footer";
 import HomePosts from "../components/HomePosts";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     // md for medium screen
-    <div className="px-8 md:px-[200px]">
-      <HomePosts />
-      <HomePosts />
-      <HomePosts />
-      <HomePosts />
-      <HomePosts />
-    </div>
+    <>
+      <Navbar />
+      <div className="px-8 md:px-[200px]">
+        <HomePosts />
+        <HomePosts />
+        <HomePosts />
+        <HomePosts />
+        <HomePosts />
+      </div>
+      <Footer />
+    </>
   );
 };
 
