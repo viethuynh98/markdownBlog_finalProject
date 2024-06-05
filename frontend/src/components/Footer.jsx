cd7425d1fa632f395fb3baaa8fad72c4c6bdecce
Viet Huynh <huynhviet98@gmail.com>
2024-06-06 00:27:54 +0700

    -config footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,30 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <div className="mt-8 w-full bg-black px-8 md:px-[200px] flex flex-col md:flex-row items-start md:items-center md:justify-between space-y-4 md:space-y-0 md:space-x-4 py-8 text-sm md:text-md">
+        <div className="flex flex-col text-white">
+          <p>Featured Blogs</p>
+          <p>Most viewed</p>
+          <p>Readers Choice</p>
+        </div>
 
-export default Footer
+        <div className="flex flex-col text-white">
+          <p>Forum</p>
+          <p>Support</p>
+          <p>Recent Posts</p>
+        </div>
+
+        <div className="flex flex-col text-white">
+          <p>Privacy Policy</p>
+          <p>Terms & Conditions</p>
+          <p>Terms of Services</p>
+        </div>
+      </div>
+      <p className="py-2 pb-2 text-center text-white bg-black">
+        All right reserved @Blog Market 2024
+      </p>
+    </>
+  );
+};
+
+export default Footer;
