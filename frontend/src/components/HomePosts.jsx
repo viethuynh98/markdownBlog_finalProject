93dd14f3026a7542fc9dcd6ba6d6ff24af096bab
Viet Huynh <huynhviet98@gmail.com>
2024-06-06 00:02:42 +0700

    -config home post design

--- frontend/src/components/HomePosts.jsx
+++ frontend/src/components/HomePosts.jsx
@@ -1,7 +1,29 @@
 const HomePosts = () => {
   return (
-    <div>HomePosts</div>
-  )
-}
+    <div className="w-full flex mt-8 space-x-4">
+      {/* left */}
+      <div className="w-[35%] h-[200px] flex justify-center items-center">
+        <img src="" alt="" className="" />
+      </div>
+      {/* right */}
+      <div className="flex flex-col w-[65%]">
+        <h1 className="text-xl font-bold md:mb-2 mb-1 md:text-2xl">
+          10 Uses of Artificial Intelligence in Day to Day Life
+        </h1>
+        <div className="flex mb-2 text-sm font-semibold text-gray-500 items-center justify-between md:mb-4">
+          <p>@VietHuynh</p>
+          <div className="flex space-x-2">
+            <p>16/06/2023</p>
+            <p>16:45</p>
+          </div>
+        </div>
+        <p className="text-sm md:text-lg">
+          Prominent examples of AI software used in everyday life include voice
+          assistant, image...
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default HomePosts
+export default HomePosts;
