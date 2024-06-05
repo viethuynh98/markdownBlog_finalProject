5940cab5fda99fec05bcad5690aa5f6b3c332a8e
Viet Huynh <huynhviet98@gmail.com>
2024-06-05 23:52:08 +0700

    - update navigation bar design

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,47 @@
 import { Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
 
 const Navbar = () => {
-  {
-    /* design Navbar */
-  }
+  const user = true;
   return (
+    // div for all
     <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
-      <h1 className="text-xl font-extrabold">
+      <h1 className="text-lg md:text-xl font-extrabold">
         <Link to="/">Blog Market</Link>
       </h1>
+
+      {/* div for search */}
+      <div className="flex justify-center items-center space-x-0">
+        <p>
+          <BsSearch />
+        </p>
+        <input
+          className="outline-none px-3"
+          placeholder="Search a post"
+          type="text"
+        />
+      </div>
+
+      {/* div for login and register */}
       <div className="flex items-center justify-center space-x-2 md:space-x-4">
-        <h3>
-          <Link to="/login">Login</Link>
-        </h3>
-        <h3>
-          <Link to="/register">Register</Link>
-        </h3>
+        {user ? (
+          <h3>
+            <Link to="/write">Write</Link>
+          </h3>
+        ) : (
+          <h3>
+            <Link to="/login">Login</Link>
+          </h3>
+        )}
+        {user ? (
+          <h3>
+            <Link to="/profile">Profile</Link>
+          </h3>
+        ) : (
+          <h3>
+            <Link to="/register">Register</Link>
+          </h3>
+        )}
       </div>
     </div>
   );
