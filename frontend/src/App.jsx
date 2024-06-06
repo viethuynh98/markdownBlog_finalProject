04c8ff99bc6aa68ecd63acc9c82f58875edd53fc
Viet Huynh <huynhviet98@gmail.com>
2024-06-06 10:22:07 +0700

    - add new route to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,21 @@ import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import PostDetails from "./pages/PostDetails";
+import CreatePost from "./pages/CreatePost";
+import EditPost from "./pages/EditPost";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/posts/post/:id" element={<PostDetails/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/write" element={<CreatePost />} />
+        <Route path="/posts/post/:id" element={<PostDetails />} />
+        <Route path="/edit/:id" element={<EditPost />} />
+        <Route path="/profile/:id" element={<Profile />} />
       </Routes>
     </div>
   );
