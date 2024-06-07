56837e1156a3fc49b2f2e81496f34850ff32da0a
Viet Huynh <huynhviet98@gmail.com>
2024-06-07 18:04:06 +0700

    - use env variable

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+// save confidential data with dotenv
+const dotenv = require("dotenv");
 
 // database
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://huynhviet98:Ymieu431811998!@clusterv98.mlflbal.mongodb.net/blog?retryWrites=true&w=majority"
-    );
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("database is connected!!!");
   } catch (err) {
     console.log(err);
   }
 };
 
-app.listen(500, () => {
+dotenv.config();
+
+app.listen(process.env.PORT, () => {
   //call
   connectDB();
-  console.log("app is running on port 5000");
+  console.log(`app is running on port ${process.env.PORT}`);
 });
