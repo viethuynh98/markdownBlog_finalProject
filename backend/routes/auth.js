48dd6a94b902dd285fbd5516f63d7d6dc9742e94
truongducthieu <truongducthieu2021@gmail.com>
2024-06-07 20:59:44 +0700

    - cau hinh authen cho login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -18,4 +18,31 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// LOG IN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json("User not found!");
+    }
+    const match = await bcrypt.compare(req.body.password, user.password);
+
+    if (!match) {
+      return res.status(401).json("Wrong credentials!");
+    }
+    const token = jwt.sign(
+      { _id: user._id, username: user.username, email: user.email },
+      process.env.SECRET,
+      { expiresIn: "3d" }
+    );
+    const { password, ...info } = user._doc;
+    res.cookie("token", token).status(200).json(info);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(200).json(err);
+  }
+});
+
+
 module.exports = router;
