50b806d6c36ee2105e94ec3d745b93d4ddee399e
NguyenBao <Baoaenhe@gmail.com>
2024-06-08 16:31:23 +0700

    + khoi tao delete comment route

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -29,3 +29,12 @@ router.put("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+// DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    await Comment.findByIdAndDelete(req.params.id);
+    res.status(200).json("Comment has been deleted!");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
