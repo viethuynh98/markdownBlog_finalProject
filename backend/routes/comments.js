82a25b009dd27bf17fe8b4f4e653029235e11747
NguyenBao <Baoaenhe@gmail.com>
2024-06-08 16:30:52 +0700

    + khoi tao create comment route

---
+++ backend/routes/comments.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const Post = require("../models/Post");
+const Comment = require("../models/Comment");
+// const verifyToken = require("../verifyToken");
+
+// CREATE
+router.post("/create", async (req, res) => {
+  try {
+    const newComment = new Comment(req.body);
+    const savedComment = await newComment.save();
+    res.status(200).json(savedComment);
+  } catch (err) {
+    res.status(200).json(err);
+  }
+});
