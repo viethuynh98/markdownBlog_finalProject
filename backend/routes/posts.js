c6373149a0b2f6077cc4c616aa7b68abfa3e0d63
NguyenBao <Baoaenhe@gmail.com>
2024-06-08 15:49:46 +0700

    + tao post update route

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -16,3 +16,16 @@ router.post("/create", async (req, res) => {
     res.status(200).json(err);
   }
 });
+// UPDATE
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
