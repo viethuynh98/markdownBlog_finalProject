ae6853713dfd0bcc18382feec774d0876fc65a78
NguyenBao <Baoaenhe@gmail.com>
2024-06-08 15:48:26 +0700

    + khoi tao post route

---
+++ backend/routes/posts.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const Post = require("../models/Post");
+const Comment = require("../models/Comment");
