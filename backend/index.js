411bdae6376b676699351c20cc7ee1c36578ecee
NguyenBao <Baoaenhe@gmail.com>
2024-06-08 15:49:14 +0700

    + them post route vao app

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv");
 
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
 
 // database
 const connectDB = async () => {
@@ -22,6 +23,7 @@ dotenv.config();
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(process.env.PORT, () => {
   //call
