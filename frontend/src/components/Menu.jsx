e45dbdc72936e9378a19b56cf021b8f8ff8e87bb
Viet Huynh <huynhviet98@gmail.com>
2024-06-09 02:10:30 +0700

    + init Menu

---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,27 @@
+const Menu = () => {
+  const user = false;
+  return (
+    <div className="bg-black w-[150px] flex flex-col items-start absolute top-10 md:top-11 right-6 md:right-16 rounded-md p-3 space-y-4">
+      {!user && (
+        <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">Login</h3>
+      )}
+      {!user && (
+        <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">Register</h3>
+      )}
+      {user && (
+        <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">Profile</h3>
+      )}
+      {user && (
+        <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">Write</h3>
+      )}
+      {user && (
+        <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">My Blog</h3>
+      )}
+      {user && (
+        <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">Logout</h3>
+      )}
+    </div>
+  );
+};
+
+export default Menu;
