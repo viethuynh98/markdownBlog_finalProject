a49ab84d65d366f5e106981354085f6544d96ca7
NguyenBao <Baoaenhe@gmail.com>
2024-06-08 15:49:35 +0700

    + tao post create route

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -4,3 +4,15 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const Post = require("../models/Post");
 const Comment = require("../models/Comment");
+// const verifyToken = require("../verifyToken");
+
+// CREATE
+router.post("/create", async (req, res) => {
+  try {
+    const newPost = new Post(res.body);
+    const savedPost = await newPost.save();
+    res.status(200).json(savesPost);
+  } catch (err) {
+    res.status(200).json(err);
+  }
+});
