9419a0a89dfabd9fceade49a1609525800b92803
NguyenBao <Baoaenhe@gmail.com>
2024-06-08 15:50:13 +0700

    + tao get post detail route

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -38,3 +38,13 @@ router.delete("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// GET POST DETAIL
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
