9ad9b431c2e6201789a0ed31bb97c12d6ff20ccc
NguyenBao <Baoaenhe@gmail.com>
2024-06-08 16:31:04 +0700

    + khoi tao update comment route

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -16,3 +16,16 @@ router.post("/create", async (req, res) => {
     res.status(200).json(err);
   }
 });
+// UPDATE
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedComment = await Comment.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
