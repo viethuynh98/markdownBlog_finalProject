59a9d7a55c6ce30535696847851967c4705c85d0
DucThieu <truongducthieu2021@gmail.com>
2024-06-09 11:25:49 +0700

    - truyen them user data cho token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -34,9 +34,13 @@ router.post("/login", async (req, res) => {
       return res.status(401).json("Wrong credentials!");
     }
     // tao token
-    const token = jwt.sign({ _id: user._id }, process.env.SECRET, {
-      expiresIn: "3d",
-    });
+    const token = jwt.sign(
+      { _id: user._id, username: user.username, email: user.email },
+      process.env.SECRET,
+      {
+        expiresIn: "3d",
+      }
+    );
     const { password, ...info } = user._doc;
     res.cookie("token", token).status(200).json(info);
     // res.status(200).json(user);
