7516e07075d3c61e075036a8bc1276af17f5e6f8
NguyenBao <Baoaenhe@gmail.com>
2024-06-09 15:09:00 +0700

    - lay du lieu post tu server

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,29 @@
+import axios from "axios";
 import Footer from "../components/Footer";
 import HomePosts from "../components/HomePosts";
 import Navbar from "../components/Navbar";
+import { URL } from "../url";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  // lay du lieu tu server
+  const fetchPosts = async () => {
+    try {
+      const res = await axios.get(URL + "/api/posts");
+      console.log(res.data);
+      setPosts(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // call
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     // md for medium screen
     <>
