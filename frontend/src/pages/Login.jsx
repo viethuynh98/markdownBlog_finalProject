2160305a471f4dd66e2bb56aa780b082e6b7bc4c
DucThieu <truongducthieu2021@gmail.com>
2024-06-09 10:34:53 +0700

    - xu ly dang nhap

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
+import { useState } from "react";
+import axios from "axios";
+import { URL } from "../url";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(URL + "/api/auth/login", {
+        email,
+        password,
+      });
+      console.log(res.data);
+      navigate("/");
+    } catch (err) {
+      setError(true);
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
@@ -19,6 +41,7 @@ const Login = () => {
             Log in to your account
           </h1>
           <input
+            onChange={(e) => setEmail(e.target.value)}
             className="w-full px-4 py-2 border-2 border-black outline-0"
             type="text"
             placeholder="Enter your email"
@@ -26,15 +49,22 @@ const Login = () => {
             id=""
           />
           <input
+            onChange={(e) => setPassword(e.target.value)}
             className="w-full px-4 py-2 border-2 border-black outline-0"
             type="text"
             placeholder="Enter your password"
             name=""
             id=""
           />
-          <button className="w-full px-4 py-4 text-lg font-bold text-white bg-black rounded-lg hover:bg-gray-500 hover:text-black">
+          <button
+            onClick={handleLogin}
+            className="w-full px-4 py-4 text-lg font-bold text-white bg-black rounded-lg hover:bg-gray-500 hover:text-black"
+          >
             Log in
           </button>
+          {error && (
+            <h3 className="text-red-500 text-sm">Something went wrong</h3>
+          )}
           <div className="flex justify-center items-center space-x-3">
             <p>New here?</p>
             <p className="text-gray-500 hover:text-black">
@@ -43,7 +73,7 @@ const Login = () => {
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 };
