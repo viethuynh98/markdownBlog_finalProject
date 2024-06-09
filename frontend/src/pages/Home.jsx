739f4ae9d97b84bb53bc6f8101ac40cdf71b6ad4
Viet Huynh <huynhviet98@gmail.com>
2024-06-09 16:18:43 +0700

    - get searched post from db

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,16 +4,27 @@ import HomePosts from "../components/HomePosts";
 import Navbar from "../components/Navbar";
 import { URL } from "../url";
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Home = () => {
+  const { search } = useLocation();
+  // console.log(search);
   const [posts, setPosts] = useState([]);
+  // check if no posts match the search filter then "nothing"
 
   // lay du lieu tu server
   const fetchPosts = async () => {
     try {
-      const res = await axios.get(URL + "/api/posts");
-      console.log(res.data);
+      // GET POSTS route to check search filter
+      const res = await axios.get(URL + "/api/posts" + search);
+      // console.log(res.data);
       setPosts(res.data);
+
+      if (res.data.length == 0) {
+        setNoResults(true);
+        } else {
+          setNoResults(false);
+      }
     } catch (err) {
       console.log(err);
     }
@@ -22,7 +33,7 @@ const Home = () => {
   // call
   useEffect(() => {
     fetchPosts();
-  }, []);
+  }, [search]);
 
   return (
     // md for medium screen
