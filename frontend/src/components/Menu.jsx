21d7478dfcfcd4c5b21ce5844bb4b1c3f68b78db
DucThieu <truongducthieu2021@gmail.com>
2024-06-09 11:53:25 +0700

    define handle log ouot function

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,8 +1,24 @@
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
+import axios from "axios";
+import { URL } from "../url";
 
 const Menu = () => {
   const { user } = useContext(UserContext);
+  const { setUser } = useContext(UserContext);
+
+  const handleLogout = async () => {
+    try {
+      const res = await axios.get(URL + "/api/auth/logout", {
+        withCredentials: true,
+      });
+      console.log(res);
+      setUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="bg-black w-[150px] flex flex-col items-start absolute top-10 md:top-11 right-6 md:right-16 rounded-md p-3 space-y-4">
       {!user && (
