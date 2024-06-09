ff1722ebcafe07a3284f87c71a1b19ae95000780
Viet Huynh <huynhviet98@gmail.com>
2024-06-09 16:28:59 +0700

    + init loader animation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,30 +5,39 @@ import Navbar from "../components/Navbar";
 import { URL } from "../url";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import Loader from "../components/Loader";
 
 const Home = () => {
   const { search } = useLocation();
   // console.log(search);
   const [posts, setPosts] = useState([]);
-  // check if no posts match the search filter then "nothing"
+  // (1) check if no posts match the search filter then "nothing"
   const [noResults, setNoResults] = useState(false);
-
+  // (2) add loader animation on loading posts
+  const [loader, setLoader] = useState(false);
 
   // lay du lieu tu server
   const fetchPosts = async () => {
+    // (2)
+    setLoader(true);
     try {
       // GET POSTS route to check search filter
       const res = await axios.get(URL + "/api/posts" + search);
       // console.log(res.data);
       setPosts(res.data);
 
+      // (1)
       if (res.data.length == 0) {
         setNoResults(true);
-        } else {
-          setNoResults(false);
+      } else {
+        setNoResults(false);
       }
+
+      // (2)
+      setLoader(false);
     } catch (err) {
       console.log(err);
+      setLoader(true);
     }
   };
 
@@ -42,7 +51,11 @@ const Home = () => {
     <>
       <Navbar />
       <div className="px-8 md:px-[200px] min-h-[80vh]">
-        {!noResults ? (
+        {loader ? ( // (2)
+          <div className="h-[40vh] flex justify-center items-center">
+            <Loader />
+          </div>
+        ) : !noResults ? ( // (1)
           posts.map((post) => <HomePosts key={post._id} post={post} />)
         ) : (
           <h3 className="text-center font-bold mt-16"> NO POSTS AVAILABLE</h3>
