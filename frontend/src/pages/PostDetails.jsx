a4ea746c297ea505a74df9eb6d7a59d374350fa6
Viet Huynh <huynhviet98@gmail.com>
2024-06-09 20:10:35 +0700

    - get post detail from db

--- frontend/src/pages/PostDetails.jsx
+++ frontend/src/pages/PostDetails.jsx
@@ -1,17 +1,39 @@
+import { useParams } from "react-router-dom";
 import Comment from "../components/Comment";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { BiEdit } from "react-icons/bi";
 import { MdDelete } from "react-icons/md";
+import axios from "axios";
+import { URL } from "../url";
+import { useEffect, useState } from "react";
 
 const PostDetails = () => {
+  const postId = useParams().id;
+  // console.log(postId);
+  const [post, setPost] = useState({});
+
+  const fetchPost = async () => {
+    try {
+      const res = await axios.get(URL + `/api/posts/${postId}`);
+      // console.log(res.data);
+      setPost(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <div className="px-8 md:px-[200px] mt-8">
         <div className="flex justify-between items-center">
           <h1 className="text-2xl font-bold text-black md:text-3xl">
-            10 Uses of Artificial Intelligence in Day to Day Life
+            {post.title}
           </h1>
           <div className="flex items-center justify-center space-x-2">
             <p>
@@ -23,41 +45,30 @@ const PostDetails = () => {
           </div>
         </div>
         <div className="flex items-center justify-between mt-2 md:mt-4">
-          <p>@VietHuynh</p>
+          <p>@{post.username}</p>
           <div className="flex space-x-2">
-            <p>16/06/2023</p>
-            <p>16:45</p>
+            <p>{new Date(post.updatedAt).toString().slice(0, 15)}</p>
+            <p>{new Date(post.updatedAt).toString().slice(16, 24)}</p>
           </div>
         </div>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCOQ4hMuGG1yUZ643COTv_FHyo9HZSaa7Zmg&s"
-          className="w-full mx-auto mt-8"
-          alt=""
-        />
-        <p className="mx-auto mt-8">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry`s standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
+        <img src={post.photo} className="w-full mx-auto mt-8" alt="" />
+        <p className="mx-auto mt-8">{post.desc}</p>
         <div className="flex items-center mt-8 space-x-4 font-semibold">
           <p>Categories:</p>
           <div className="flex justify-center items-center space-x-2">
-            <div className="bg-gray-500 rounded-lg px-3 py-1">Technology</div>
-            <div className="bg-gray-500 rounded-lg px-3 py-1">AI</div>
+            {post.categories.map((c, i) => (
+              <div key={i} className="bg-gray-500 rounded-lg px-3 py-1">
+                {c}
+              </div>
+            ))}
           </div>
         </div>
         <div className="flex flex-col mt-4">
           <h3 className="mt-6 mb-2 font-semibold">Comments:</h3>
           {/* comment */}
-          <Comment/>
-          <Comment/>
-          <Comment/>
+          <Comment />
+          <Comment />
+          <Comment />
         </div>
         {/* write a commnent */}
         <div className="flex flex-col mt-4 md:flex-row">
