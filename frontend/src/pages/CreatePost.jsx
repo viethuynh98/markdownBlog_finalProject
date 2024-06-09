1c1e26504a9a3d766ba969aa80674af598ddc16d
NguyenBao <Baoaenhe@gmail.com>
2024-06-09 23:22:40 +0700

    - update create post to save data into db

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { ImCross } from "react-icons/im";
+import { UserContext } from "../context/UserContext";
+import axios from "axios";
+import { URL } from "../url";
+import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [file, setFile] = useState(null);
+  const { user } = useContext(UserContext);
   const [cat, setCat] = useState("");
   const [cats, setCats] = useState([]);
+
+    const navigate = useNavigate();
 
   const addCategory = () => {
     if (cat.trim() === "") return; // Không thêm nếu input trống
@@ -21,6 +31,44 @@ const CreatePost = () => {
     setCats(updatedCats);
   };
 
+  const handleCreate = async (e) => {
+    e.preventDefault();
+    const post = {
+      title,
+      desc,
+      username: user.username,
+      userId: user._id,
+      categories: cats,
+    };
+
+    if (file) {
+      const data = new FormData();
+      const filename = Date.now() + file.name;
+      data.append("img", filename);
+      data.append("file", file);
+      post.photo = filename;
+
+      // image upload
+      try {
+        const imgUpload = await axios.post(URL + "/api/upload", data);
+        // console.log(imgUpload.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    // post upload
+    try {
+      const res = await axios.post(URL + "/api/posts/create", post, {
+        withCredentials: true,
+      });
+      navigate("/posts/post/" + res.data._id);
+      // console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       <Navbar />
@@ -28,11 +76,16 @@ const CreatePost = () => {
         <h1 className="font-bold md:text-2xl text-xl">Create a post</h1>
         <form className="w-full flex flex-col space-y-4 md:space-y-8 mt-4">
           <input
+            onChange={(e) => setTitle(e.target.value)}
             type="text"
             placeholder="Enter post title"
             className="px-4 py-2 outline-1"
           />
-          <input type="file" className="px-4" />
+          <input
+            onChange={(e) => setFile(e.target.files[0])}
+            type="file"
+            className="px-4"
+          />
 
           {/* category */}
           <div className="flex flex-col">
@@ -76,6 +129,7 @@ const CreatePost = () => {
           </div>
 
           <textarea
+            onChange={(e) => setDesc(e.target.value)}
             name=""
             id=""
             rows={15}
@@ -83,7 +137,10 @@ const CreatePost = () => {
             placeholder="Enter post description"
             className="px-4 py-2 outline-none"
           />
-          <button className="bg-black w-4/12 md:w-1/5 mx-auto text-white font-semibold px-4 py-2 md:text-xl text-lg">
+          <button
+            onClick={handleCreate}
+            className="bg-black w-4/12 md:w-1/5 mx-auto text-white font-semibold px-4 py-2 md:text-xl text-lg"
+          >
             Create
           </button>
         </form>
