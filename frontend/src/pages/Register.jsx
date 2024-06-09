e087b473e2374137b9f35a1fb65e31bb1a9b67ef
Viet Huynh <huynhviet98@gmail.com>
2024-06-09 10:23:44 +0700

    - handle register functionality

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,8 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
+import { useState } from "react";
+import axios from "axios";
+import { URL } from "../url";
 
 const Register = () => {
   const [username, setUsername] = useState("");
@@ -7,6 +10,32 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
   const navigate = useNavigate();
+
+  const handleRegister = async () => {
+    try {
+      const res = await axios.post(URL + "/api/auth/register", {
+        username,
+        email,
+        password,
+      });
+      setUsername(res.data.username);
+      setEmail(res.data.email);
+      setPassword(res.data.password);
+      setError(false);
+      navigate("/login");
+
+      console.log(res);
+    } catch (err) {
+      setError(true);
+      console.log(err);
+    }
+  };
+
+  // test
+  // console.log(username);
+  // console.log(email);
+  // console.log(password);
+
   return (
     <>
       <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
@@ -45,9 +74,13 @@ const Register = () => {
             name=""
             id=""
           />
-          <button className="w-full px-4 py-4 text-lg font-bold text-white bg-black rounded-lg hover:bg-gray-500 hover:text-black">
+          <button
+            onClick={handleRegister}
+            className="w-full px-4 py-4 text-lg font-bold text-white bg-black rounded-lg hover:bg-gray-500 hover:text-black"
+          >
             Register
           </button>
+          {error && <h3 className="text-red-500 text-sm">Something went wrong</h3>}
           <div className="flex justify-center items-center space-x-3">
             <p>Already have an account?</p>
             <p className="text-gray-500 hover:text-black">
