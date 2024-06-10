08a6cda47fc9e88c07071ebbcf57996d7b766bc0
Viet Huynh <huynhviet98@gmail.com>
2024-06-10 11:41:12 +0700

    + init my blogs page

---
+++ frontend/src/components/MyBlogs.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer"
+import Navbar from "./Navbar"
+
+const MyBlogs = () => {
+  return (
+    <div>
+      <Navbar/>
+
+      <Footer/>
+    </div>
+  )
+}
+
+export default MyBlogs
