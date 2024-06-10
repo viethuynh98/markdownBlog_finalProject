0a90e171a8370c2e9be85992b21048a6b69590f5
Viet Huynh <huynhviet98@gmail.com>
2024-06-11 03:25:41 +0700

    - update profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -19,8 +19,6 @@ const Profile = () => {
   const [posts, setPosts] = useState([]);
   const [updated, setUpdated] = useState(false);
   const [profile, setProfile] = useState("");
-  // console.log(user)
-  // console.log(param)
 
   const fetchUserCredentials = async () => {
     try {
@@ -43,7 +41,6 @@ const Profile = () => {
         { username, email, password },
         { withCredentials: true }
       );
-      // console.log(res.data)
       setUpdated(true);
     } catch (err) {
       console.log(err);
@@ -58,16 +55,14 @@ const Profile = () => {
       });
       setUser(null);
       navigate("/");
-      // console.log(res.data)
     } catch (err) {
       console.log(err);
     }
   };
-  // console.log(user)
+
   const fetchUserPosts = async () => {
     try {
       const res = await axios.get(URL + "/api/posts/user/" + param);
-      // console.log(res.data)
       setPosts(res.data);
     } catch (err) {
       console.log(err);
@@ -77,7 +72,6 @@ const Profile = () => {
   const fetchUserProfile = async () => {
     try {
       const res = await axios.get(URL + "/api/userProfile/" + param);
-      console.log(res.data[0])
       setProfile(res.data[0]);
     } catch (err) {
       console.log(err);
@@ -99,51 +93,39 @@ const Profile = () => {
   return (
     <div>
       <Navbar />
-      <div className="min-h-[80vh] px-8 md:px-[200px] mt-8 flex md:flex-row flex-col-reverse md:items-start items-start">
-        <div className="flex flex-col md:w-[70%] w-full mt-8 md:mt-0">
-          <h1 className="text-xl font-bold mb-4">Your posts:</h1>
+      <div className="min-h-[80vh] px-8 md:px-[200px] mt-8 flex flex-col items-start space-y-8">
+        {/* Summary */}
+        <div className="w-full p-4 border shadow-md">
+          <h1 className="text-xl font-bold mb-4">SUMMARY</h1>
+          <p>{profile?.summary}</p>
+        </div>
+        {/* Skills */}
+        <div className="w-full p-4 border shadow-md">
+          <h1 className="text-xl font-bold mb-4">SKILLS</h1>
+          {profile?.skills?.map((skill, index) => (
+            <p key={index}>{skill}</p>
+          ))}
+        </div>
+        {/* Education */}
+        <div className="w-full p-4 border shadow-md">
+          <h1 className="text-xl font-bold mb-4">EDUCATION</h1>
+          {profile?.educations?.map((education, index) => (
+            <p key={index}>{education}</p>
+          ))}
+        </div>
+        {/* Interests */}
+        <div className="w-full p-4 border shadow-md">
+          <h1 className="text-xl font-bold mb-4">INTERESTS</h1>
+          {profile?.interests?.map((interest, index) => (
+            <p key={index}>{interest}</p>
+          ))}
+        </div>
+        {/* Posts */}
+        <div className="w-full p-4 border shadow-md">
+          <h1 className="text-xl font-bold mb-4">POSTS</h1>
           {posts?.map((p) => (
             <ProfilePosts key={p._id} p={p} />
           ))}
-        </div>
-        <div className="md:sticky md:top-12  flex justify-start md:justify-end items-start md:w-[30%] w-full md:items-end ">
-          <div className=" flex flex-col space-y-4 items-start">
-            <h1 className="text-xl font-bold mb-4">Profile</h1>
-            <input
-              onChange={(e) => setUsername(e.target.value)}
-              value={username}
-              className="outline-none px-4 py-2 text-gray-500"
-              placeholder="Your username"
-              type="text"
-            />
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              className="outline-none px-4 py-2 text-gray-500"
-              placeholder="Your email"
-              type="email"
-            />
-            {/* <input onChange={(e)=>setPassword(e.target.value)} value={password} className="outline-none px-4 py-2 text-gray-500" placeholder="Your password" type="password"/> */}
-            <div className="flex items-center space-x-4 mt-8">
-              <button
-                onClick={handleUserUpdate}
-                className="text-white font-semibold bg-black px-4 py-2 hover:text-black hover:bg-gray-400"
-              >
-                Update
-              </button>
-              <button
-                onClick={handleUserDelete}
-                className="text-white font-semibold bg-black px-4 py-2 hover:text-black hover:bg-gray-400"
-              >
-                Delete
-              </button>
-            </div>
-            {updated && (
-              <h3 className="text-green-500 text-sm text-center mt-4">
-                user updated successfully!
-              </h3>
-            )}
-          </div>
         </div>
       </div>
       <Footer />
@@ -152,3 +134,47 @@ const Profile = () => {
 };
 
 export default Profile;
+
+{
+  /* <input onChange={(e)=>setPassword(e.target.value)} value={password} className="outline-none px-4 py-2 text-gray-500" placeholder="Your password" type="password"/> */
+}
+{
+  /* <div className="md:sticky md:top-12  flex justify-start md:justify-end items-start md:w-[30%] w-full md:items-end ">
+<div className=" flex flex-col space-y-4 items-start">
+  <h1 className="text-xl font-bold mb-4">Profile</h1>
+  <input
+    onChange={(e) => setUsername(e.target.value)}
+    value={username}
+    className="outline-none px-4 py-2 text-gray-500"
+    placeholder="Your username"
+    type="text"
+  />
+  <input
+    onChange={(e) => setEmail(e.target.value)}
+    value={email}
+    className="outline-none px-4 py-2 text-gray-500"
+    placeholder="Your email"
+    type="email"
+  />
+  <div className="flex items-center space-x-4 mt-8">
+    <button
+      onClick={handleUserUpdate}
+      className="text-white font-semibold bg-black px-4 py-2 hover:text-black hover:bg-gray-400"
+    >
+      Update
+    </button>
+    <button
+      onClick={handleUserDelete}
+      className="text-white font-semibold bg-black px-4 py-2 hover:text-black hover:bg-gray-400"
+    >
+      Delete
+    </button>
+  </div>
+  {updated && (
+    <h3 className="text-green-500 text-sm text-center mt-4">
+      user updated successfully!
+    </h3>
+  )}
+</div>
+</div> */
+}
